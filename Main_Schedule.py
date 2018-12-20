309b112865b23813f26c0225b8c00e3b4121bd98
Aman Riat <amanriat777@gmail.com>
2018-12-19 19:11:35 -0500

    Encapsulation and layout

--- Main_Schedule.py
+++ Main_Schedule.py
@@ -1,19 +1,32 @@
-import pandas as pd 
- 
-data = pd.read_csv("nba.csv")
+from pandas import DataFrame, read_csv
+import pandas as pd
+import xlrd
 
-print("Total rows: {}".format(len(data)))
+#Reading Spreadsheet Data 
+file = r'NBA_18_19.xls'
+data = pd.read_excel(file)
 print (list(data))
 
-
 #Checking if Game(s) entered is valid 
-def Game_Range(b):
-    for x in data['Date']:
-        for y in range (0,(len(data['Date']))):
-            if "16/10/2018" in data['Date'][y]:
-                return True 
+def Game_Range(game_start):
+    for date in data['Date']:
+        for date_index in range (0,(len(data['Date']))):
+            if game_start in data['Date'][date_index]:
+                return True, date_index
             else:
-                print ('Nah')
+                pass
 
-game = input("Enter a starting game:")
+            
+#Checking the games played 
+def Games_Played(game_start,game_end):
+    counter = 0
+    print (type(data['Atl'][1]))
+    if (type(data['Atl'][1])) == str:
+        counter += 1
+    print (counter)
+
+    
+game = input("Enter a starting game: ")
 print (Game_Range(game))
+
+
