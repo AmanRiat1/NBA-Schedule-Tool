ceb9793ea264ad91ad1c1ce5df8ebe34caaae172
Aman Riat <amanriat777@gmail.com>
2018-12-22 15:08:14 -0500

    Finished the crucial parts, need to format output now

--- Main_Schedule.py
+++ Main_Schedule.py
@@ -26,7 +26,7 @@ def Game_Position(game):
     for date in data['Date']:
         for date_index in range (0,(len(data['Date']))):
             if game in data['Date'][date_index]:
-                return game
+                return date_index
             else:
                 pass
     
@@ -35,19 +35,58 @@ def Game_Position(game):
 #Checking the games played 
 def Games_Played(game_start,game_end):
 
+    start = int(game_start)
+    end = int(game_end)
+
     #New list with all NBA Teams
     nba_t = (list(data)[1:31])
 
-    #TODO: Fix loop so it iterates over 30 teams     
-    counter = -1
-    for team in data[nba_t[5]]:
-        if type(team) == str:
-            counter += 1
+    #Lists that will show how many games teams are playing
+    five = []
+    four = []
+    three = []
+    two = []
+    one = []
+    zero = []
+
+    #Variable used as counter to retrieve position of team in list
+    team = 0
+    
+    #Loop iterates over 30 teams
+    while team <= 29:
+        counter = 0
+        team_sch = (list(data[nba_t[team]]))
+
+        #For loop iterates over game range user enters 
+        for game in range (start,end+1):
+            if type(team_sch[game]) == str:
+                counter += 1
+            else:
+                pass
+
+        if counter == 5:
+            five.append(nba_t[team])
+        elif counter == 4:
+            four.append(nba_t[team])
+        elif counter == 3:
+            three.append(nba_t[team])
+        elif counter == 2:
+            two.append(nba_t[team])
+        elif counter == 1:
+            one.append(nba_t[team])
         else:
-            pass
+            zero.append(nba_t[team])
+            
+            
+        team += 1
 
-    print (counter)
-
+        
+    print ("Teams with five games:", five)
+    print ("Teams with four games:", four)
+    print ("Teams with three games:", three)
+    print ("Teams with two games:", two)
+    print ("Teams with one game:", one)
+    print ("Teams that have no games:", zero)
 
 
 #Main 
@@ -60,6 +99,8 @@ start,end = Game_Checker(game_start),Game_Checker(game_end)
 if start and end == True:
     Start_Pos = Game_Position(game_start)
     End_Pos = Game_Position(game_end)
+    print (Start_Pos)
+    print (End_Pos)
     
     #Counting Games
     Total_games = Games_Played(Start_Pos,End_Pos)
--- tester.py
+++ tester.py
@@ -24,20 +24,38 @@ zero = []
 while index <= 29:
     counter = 0
     team_sch = (list(data[teams[index]]))
-    print (teams[index])
+
 
     #For loop iterates over game range user enters 
-    for game in range (1):
+    for game in range (0,7):
         if type(team_sch[game]) == str:
             counter += 1
         else:
             pass
 
-    if counter == 1:
+    if counter == 5:
+        five.append(teams[index])
+    elif counter == 4:
+        four.append(teams[index])
+    elif counter == 3:
+        three.append(teams[index])
+    elif counter == 2:
+        two.append(teams[index])
+    elif counter == 1:
         one.append(teams[index])
+    else:
+        zero.append(teams[index])
+        
         
     index += 1
-print (one)
+
+    
+print ("Teams with five games", five)
+print ("Teams with four games", four)
+print ("Teams with three games", three)
+print ("Teams with two games", two)
+print ("Teams with one game", one)
+print ("Teams that have no games", zero)
 
         
 print (counter)
