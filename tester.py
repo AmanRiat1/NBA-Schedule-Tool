27797b6a495c1a3ab4a0b8caa0c597aa787619bc
Aman Riat <amanriat777@gmail.com>
2018-12-22 14:20:45 -0500

    Tester file has near completed game total function

--- tester.py
+++ tester.py
@@ -5,19 +5,42 @@ import xlrd
 #Reading Spreadsheet Data 
 file = r'NBA_18_19.xls'
 data = pd.read_excel(file)
-print (list(data))
-print (len(list(data)))
-print (list(data)[30])
+
 
 #Testing Function that will iterate over every team on certains dates
 teams = (list(data)[1:31])
+index = 0
 
-counter = -1
-for hey in data[teams[5]]:
-    if type(hey) == str:
-        counter += 1
-    else:
-        pass
 
+five = []
+four = []
+three = []
+two = []
+one = []
+zero = []
+
+
+
+while index <= 29:
+    counter = 0
+    team_sch = (list(data[teams[index]]))
+    print (teams[index])
+
+    #For loop iterates over game range user enters 
+    for game in range (1):
+        if type(team_sch[game]) == str:
+            counter += 1
+        else:
+            pass
+
+    if counter == 1:
+        one.append(teams[index])
+        
+    index += 1
+print (one)
+
+        
 print (counter)
+
+
 
